506238f61bdd43c569dbf410d997fe1185b90b5c
Sergiy <sergiy_007@ukr.net>
2016-04-08 11:34:55 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@
 #
 
 default['wowza_file'] = "WowzaStreamingEngine-4.4.1-linux-x64-installer.run"
-default['wowza_download_path'] = "http://www.wowza.com/downloads/WowzaStreamingEngine-4-4-1"
+default['wowza_download_path'] = "WowzaStreamingEngine-4-4-1"
 default['user_name'] = "admin"
 default['password'] = "admin"
 default['license_key'] = "XXX-XXXX-XXXX-XXXX-XXXX-XXXX-XXXXXX"
